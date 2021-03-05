e61114ae1b198446330242723feb296fb52ad74f
Eddie Cuellar <heriberto.cuellar1329@gmail.com>
2021-03-05 00:06:54 -0600

    centered navbar and transparent

--- portfolio/src/Components/NavBar.js
+++ portfolio/src/Components/NavBar.js
@@ -1,19 +1,21 @@
 import React from 'react';
-import { Nav, Navbar } from 'react-bootstrap';
+import { Container, Nav, Navbar } from 'react-bootstrap';
 
 
 function NavBar() {
     return (
-        <Navbar collapseOnSelect expand="sm" bg="dark" variant="dark">
-            <Navbar.Toggle className="navbar-right" aria-controls="responsive-navbar-nav" />
-            <Navbar.Collapse id="responsive-navbar-nav">
-                <Nav className="mr-auto navbar-center">
-                    <Nav.Link href="#features">About Me</Nav.Link>
-                    <Nav.Link href="#pricing">Projects</Nav.Link>
-                    <Nav.Link href="#contact">Contact</Nav.Link>
-                </Nav>
-            </Navbar.Collapse>
-        </Navbar>
+        <Container className="navbar-container">
+            <Navbar>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="mr-auto">
+                        <Nav.Link className="navbar-link" href="#features">About Me</Nav.Link>
+                        <Nav.Link className="navbar-link" href="#pricing">Projects</Nav.Link>
+                        <Nav.Link className="navbar-link" href="#contact">Contact</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+        </Container>
     )
 }
 
